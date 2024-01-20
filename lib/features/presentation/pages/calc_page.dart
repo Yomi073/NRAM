df0c4c303fd6bc3ebca017068964627d1e4a4a28
Martina Vukanovic <vukanovicmartina01@gmail.com>
2024-01-20 17:27:04 +0100

    Continuation of work on the calculator

--- lib/features/presentation/pages/calc_page.dart
+++ lib/features/presentation/pages/calc_page.dart
@@ -3,22 +3,24 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pv_smart_click/features/presentation/widgets/my_button.dart';
-import 'package:pv_smart_click/features/presentation/widgets/textfield.dart';
 
 import 'package:http/http.dart' as http;
+
+import '../../data/repository/auth_token_provider.dart';
 
 class CalculatorPage extends StatefulWidget {
   @override
   _CalculatorPageState createState() => _CalculatorPageState();
 }
 
-Future<dynamic> fetchSupplier(queryParams) async {
 
+
+Future<dynamic> fetchSupplier(queryParams) async {
   final response = await http
       .get(Uri.parse('https://dev.backend.pvsmartclick.com/price/supplierName').replace(queryParameters: queryParams),
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer xxx', // Example: Authorization header with a bearer token
+      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJmaXJzdE5hbWUiOiJNaWpvIiwibGFzdE5hbWUiOiJLb3ppbmEiLCJ1c2VySWQiOiI2NTVkMDdkMTdhOWM3NjIwNGU5NGY3YTAiLCJlbWFpbCI6Im1pam9rb3ppbmFAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9CVVNJTkVTUyJdLCJpYXQiOjE3MDU3NTgzMTUsImV4cCI6MTcwNTg0NDcxNX0.8pDnrYetoayDyj5_88obdGWGshCZxr3-l-YiEuigm1Kfkn7Bk7h53rCSo3ke9TW2RI1wBQtJVFpu2YGlTGxj0g', // Example: Authorization header with a bearer token
     },
   );
 
@@ -30,44 +32,89 @@ Future<dynamic> fetchSupplier(queryParams) async {
 }
 
 
-Future<dynamic> fetchData() async {
-  final List<Map<String, String>> queryParamsList = [
-    {'country': 'HR', 'privacyStatus': 'PUBLIC_ALL'},
-    {'country': 'BA', 'privacyStatus': 'PUBLIC_ALL'},
-  ];
-
-  List<Map<String, dynamic>> _results = [];
-
-  for (var queryParams in queryParamsList) {
-    try {
-      dynamic result = await fetchSupplier(queryParams);
-
-      // Parse the JSON string
-      List<String> data = jsonDecode(result);
-
-      _results.add({queryParams['country']!: data});
-
-    } catch (e) {
-      print('Error fetching supplier: $e');
-    }
-  }
-
-  print(_results.runtimeType);
-
-  print(_results);
-  return _results;
-  // [{HR: [{"name":"HEP ELEKTRA"},{"name":"UREDBA VLADE"}]}, {BA: [{"name":"JP Elektroprivreda HZHB"},{"name":"JP Elektroprivreda BIH"},{"name":"JP Elektroprivreda Republike Srpske"}]}
-
-
-}
-
-
 class _CalculatorPageState extends State<CalculatorPage> {
+  List<Map<String, dynamic>> fetchedData = [];
+  List<String> hrList = [];
+  List<String> baList = [];
+  String countryCode = "";
+  List<String> possibleTariffModels = [''];
+
   @override
   void initState() {
     super.initState();
     fetchData();
   }
+
+  Future<dynamic> fetchData() async {
+    final List<Map<String, String>> queryParamsList = [
+      {'country': 'HR', 'privacyStatus': 'PUBLIC_ALL'},
+      {'country': 'BA', 'privacyStatus': 'PUBLIC_ALL'},
+    ];
+
+    List<Map<String, dynamic>> _results = [];
+
+    for (var queryParams in queryParamsList) {
+      try {
+        dynamic result = await fetchSupplier(queryParams);
+
+        if (result is String) {
+          List<dynamic> decodedResult = json.decode(result);
+
+          List<String> namesList = decodedResult.map((item) => item['name'].toString()).toList();
+          _results.add({queryParams['country']!: namesList});
+        } else {
+          print("Unexpected result type: ${result.runtimeType}");
+        }
+
+      } catch (e) {
+        print('Error fetching supplier: $e');
+      }
+
+      setState(() {
+        fetchedData = _results;
+        hrList = fetchedData
+            .where((map) => map.containsKey("HR"))
+            .map((map) => map["HR"] as List<String>)
+            .fold<List<String>>([], (prev, element) => prev..addAll(element));
+
+        baList = fetchedData
+            .where((map) => map.containsKey("BA"))
+            .map((map) => map["BA"] as List<String>)
+            .fold<List<String>>([], (prev, element) => prev..addAll(element));
+      });
+    }
+
+    return _results;
+  }
+
+  Future<dynamic> fetchGroup(country, name, privacyStatus) async {
+    final response = await http
+        .get(Uri.parse('https://dev.backend.pvsmartclick.com/price/tariffName').replace(queryParameters: {
+      'country': country,
+      'name': name,
+      'privacyStatus': privacyStatus,
+    }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJmaXJzdE5hbWUiOiJNaWpvIiwibGFzdE5hbWUiOiJLb3ppbmEiLCJ1c2VySWQiOiI2NTVkMDdkMTdhOWM3NjIwNGU5NGY3YTAiLCJlbWFpbCI6Im1pam9rb3ppbmFAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9CVVNJTkVTUyJdLCJpYXQiOjE3MDU3NTEyMDYsImV4cCI6MTcwNTgzNzYwNn0.BnrGHlnMI4zC_z-29IfUsP4QBCb9B0by109cC5skat3k5Sv6XIJ5xTc5sugLdo5YI7-9HatJybnc-FtgXMu5KQ', // Example: Authorization header with a bearer token
+      },
+    );
+
+    String jsonString = response.body;
+    List<Map<String, dynamic>> jsonList = (json.decode(jsonString) as List<dynamic>).cast<Map<String, dynamic>>();
+    List<String> tariffModels = jsonList.map((item) => item['tariffModel'].toString()).toList();
+    tariffModels.add('');
+    setState(() {
+      possibleTariffModels = tariffModels;
+    });
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to load supplier');
+    }
+  }
+
 
   final country = TextEditingController();
   final supplier = TextEditingController();
@@ -91,15 +138,22 @@ class _CalculatorPageState extends State<CalculatorPage> {
 
   String? selectedSupplier = '';
 
+  String? selectedTariffGroup = '';
 
+
+  updateOptionsForThirdDropdown() async {
+    await fetchGroup(countryCode, selectedSupplier, "PUBLIC_ALL");
+  }
   void updateOptionsForSecondDropdown() {
     if (selectedCountry == 'Croatia') {
       setState(() {
-        suppliers = ['', 'a A', 'v B', 'c C'];
+        countryCode = "HR";
+        suppliers = [ ...hrList, ''];
       });
     } else if (selectedCountry == 'Bosnia and Herzegovina') {
       setState(() {
-        suppliers = ['', 'Option A', 'Option B', 'Option C'];
+        countryCode = "BA";
+        suppliers = [...baList, ''];
       });
     } else {
       setState(() {
@@ -111,12 +165,12 @@ class _CalculatorPageState extends State<CalculatorPage> {
   double _currentSliderValue = 20;
 
   void calculate(BuildContext context) {
+
     Navigator.pushReplacementNamed(context, '/result');
   }
 
   double _currentSliderValueOrientation = 0;
 
-  // Define orientation labels
   List<String> orientationLabels = [
     'N',
     'NE',
@@ -149,12 +203,11 @@ class _CalculatorPageState extends State<CalculatorPage> {
                     style: Theme.of(context).textTheme.displayMedium,
                   ),
                   const SizedBox(height: 30.0),
-
-                  const SizedBox(height: 30.0),
                   Text(
                     'Country',
                     style: Theme.of(context).textTheme.displaySmall,
                   ),
+
                   DropdownButton(
                       menuMaxHeight: 300,
                       value: selectedCountry,
@@ -162,8 +215,9 @@ class _CalculatorPageState extends State<CalculatorPage> {
                           .map((item) => DropdownMenuItem(
                           value: item,
                           child: Text(
-                              item,
-                              style: TextStyle(fontSize: 22))
+                            item.toString(),
+                            style: Theme.of(context).textTheme.displaySmall,
+                          ),
                       ))
                           .toList(),
                       onChanged: (item) => {
@@ -171,38 +225,71 @@ class _CalculatorPageState extends State<CalculatorPage> {
                           selectedCountry = item;
                           updateOptionsForSecondDropdown();
                       })
-                  }),
+                  }),const SizedBox(height: 30.0),
                   Text(
                     'Supplier',
                     style: Theme.of(context).textTheme.displaySmall,
                   ),
-                  DropdownButton(
-                      menuMaxHeight: 300,
+              SingleChildScrollView(
+                child: DropdownButton(
+                      menuMaxHeight: 400,
                       value: selectedSupplier,
+                      isExpanded: true,
                       items: suppliers
                           .map((item) => DropdownMenuItem(
-                          value: item,
-                          child: Text(
-                              item,
-                              style: TextStyle(fontSize: 22))
+                        value: item,
+                          child: ListTile(
+                            title: Text(
+                              item.toString(),
+                              style: Theme.of(context).textTheme.displaySmall,
+                            )),
                       ))
                           .toList(),
-                      onChanged: (item) => setState(() => selectedSupplier = item)
+                      onChanged: (item) => setState (() {
+                        selectedSupplier = item;
+                        updateOptionsForThirdDropdown();
+                      })
+                  )),
+                  const SizedBox(height: 30.0),
+                  Text(
+                    'Tariff group',
+                    style: Theme.of(context).textTheme.displaySmall,
                   ),
-                  const SizedBox(height: 20.0),
-                  MyTextField(
-                    controller: tariffGroup,
-                    labelText: 'Tariff group',
-                    hintText: 'Select a tariff group',
-                    obscureText: false,
-                  ),
-                  const SizedBox(height: 20.0),
+                  const SizedBox(height: 10.0),
+                DropdownButton(
+                    menuMaxHeight: 400,
+                    value: selectedTariffGroup,
+                    isExpanded: true,
+                    items: possibleTariffModels
+                        .map((item) => DropdownMenuItem(
+                      value: item,
+                      child: ListTile(
+                        title: Text(
+                        item.toString(),
+                        style: Theme.of(context).textTheme.displaySmall,
+                      )),
+                    ))
+                        .toList(),
+                    onChanged: (item) => setState (() {
+                      selectedTariffGroup = item;
+                    })
+                ),
+                  const SizedBox(height: 30.0),
                   Text(
                     'Roof Pitch',
                     style: Theme.of(context).textTheme.displaySmall,
                   ),
                   const SizedBox(height: 10.0),
-                  Slider(
+              SliderTheme(
+                data: SliderThemeData(
+                  trackHeight: 5.0, // Set the height of the track
+                  activeTrackColor: Theme.of(context).focusColor, // Set the background color of the slider
+                  inactiveTrackColor: Theme.of(context).shadowColor, // Set the color of the inactive portion of the slider
+                  thumbColor: Theme.of(context).hintColor, // Set the color of the thumb
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 9.0), // Set the shape of the thumb
+                  overlayShape: RoundSliderOverlayShape(overlayRadius: 15.0), // Set the shape of the overlay
+                ),
+                  child: Slider(
                     value: _currentSliderValue,
                     max: 180,
                     divisions: 180,
@@ -212,13 +299,23 @@ class _CalculatorPageState extends State<CalculatorPage> {
                         _currentSliderValue = value;
                       });
                     },
-                  ),
+                  )),
+                  const SizedBox(height: 30.0),
                   Text(
                     'Orientation',
                     style: Theme.of(context).textTheme.displaySmall,
                   ),
                   const SizedBox(height: 10.0),
-                  Slider(
+              SliderTheme(
+                data: SliderThemeData(
+                  trackHeight: 5.0, // Set the height of the track
+                  activeTrackColor: Theme.of(context).focusColor, // Set the background color of the slider
+                  inactiveTrackColor: Theme.of(context).shadowColor, // Set the color of the inactive portion of the slider
+                  thumbColor: Theme.of(context).hintColor, // Set the color of the thumb
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 9.0), // Set the shape of the thumb
+                  overlayShape: RoundSliderOverlayShape(overlayRadius: 15.0), // Set the shape of the overlay
+                ),
+                child: Slider(
                     value: _currentSliderValueOrientation,
                     max: orientationLabels.length.toDouble() - 1, // Adjust the maximum value based on the number of labels
                     divisions: orientationLabels.length - 1,
@@ -228,7 +325,7 @@ class _CalculatorPageState extends State<CalculatorPage> {
                         _currentSliderValueOrientation = value;
                       });
                     },
-                  ),
+                  )),
                   const SizedBox(height: 20.0),
                   TextField(
                     controller: area,
